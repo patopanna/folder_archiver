f0f25e5e88b892e3f86313264e16d68325344318
patopanna <andoxu.sss01@gmail.com>
2025-11-24 17:39:23 +0900

    first commit

---
+++ src/mac_linux.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+get_date=$(date +%Y%m%d)
+zip_file="MyWorkspace_${get_date}.zip"
+folder_path="/home/$(whoami)/MyWorkspace"
+archive_path="/home/$(whoami)/archive"
+
+# MyWorkspaceフォルダをzip圧縮して保存
+zip -r "${archive_path}/${zip_file}" "$folder_path"
+
+# 新しいzipファイルを3つ残し、それ以外を削除
+ls -1t "${archive_path}"/*.zip | tail -n +4 | xargs rm -f
